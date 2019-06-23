8ad98296a79605cf1df021ce311249d2f63506af
Javier Mansilla <javier.mansilla@mercadolibre.com>
2019-06-23 17:01:25 -0300

    0.1 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ipypy',
-    version='1.0.0',
+    version='0.1',
     url='',
     description='ipypy notebook format',
     packages=find_packages(),
