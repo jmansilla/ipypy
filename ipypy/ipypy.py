a235693a50f2b143410e63f1bbda96f28a473a64
Javier Mansilla <javier.mansilla@mercadolibre.com>
2019-06-23 14:51:41 -0300

    cleaned up json formating

--- ipypy/ipypy.py
+++ ipypy/ipypy.py
@@ -8,12 +8,17 @@ class IpypyManager(FileContentsManager):
     """
     ContentsManager that persists to a pure python file.
     """
+    allow_hidden = False
+    extension = '.nbdata'
+
     def guess_type(self, path, allow_directory=True):
         """
         Guess the type of a file.
         If allow_directory is False, don't consider the possibility that the
         file is a directory.
         """
+        if path.endswith(self.extension):
+            return 'notebook_data'
         if path.endswith('.ipynb'):
             return 'notebook'
         elif allow_directory and self.dir_exists(path):
@@ -22,33 +27,51 @@ class IpypyManager(FileContentsManager):
             return 'file'
 
     def _get_outputs_uri(self, path):
-        return path + '.nboutputs'
+        return path + self.extension
 
     def _split_model(self, model):
         shallow_model = deepcopy(model)
-        print(shallow_model.keys())
         outputs = {'type': 'file', 'format': 'text', 'content': {}}
-        prefix = 'shasha'
+        prefix = 'some-hash'
         for i, cell in enumerate(shallow_model['content']['cells']):
             if not cell.get('outputs', []):
                 continue
             key = f'{prefix}-{i}'
             outputs['content'][key] = cell['outputs']
-            cell['outputs'] = [{'id': key, 'output_type': 'external'}]
-        
+            cell['outputs'] = self._build_external_output(key)
         outputs['content'] = json.dumps(
             outputs['content'], sort_keys=True, indent=4)
         return shallow_model, outputs
+    
+    @staticmethod
+    def _get_external_output_id(cell):
+        if not cell['outputs']:
+            return None
+        out_0 = cell['outputs'][0]
+        if out_0['output_type'] == 'external':
+            return out_0.get('metadata', {}).get('id')
+        # if out_0['output_type'] == 'stream':
+        #     return out_0.get('metadata', {}).get('external_id')
+        return None
+
+    @staticmethod
+    def _build_external_output(key):
+        return [
+            {'output_type': 'external',
+             'metadata': {'id': key}
+            }
+            # {'output_type': 'stream', 'text': '',
+            #  'metadata': {'external_id': key}
+            # }
+        ]
 
     def _merge_model(self, shallow_model, outputs_model):
         model = deepcopy(shallow_model)
         outputs = json.loads(outputs_model.get('content', '{}'))
         for cell in model['content']['cells']:
-            if not cell['outputs']:
+            key = self._get_external_output_id(cell)
+            if key is None:
                 continue
-            if cell['outputs'][0]['output_type'] != 'external':
-                continue
-            key = cell['outputs'][0]['id']
             cell['outputs'] = outputs.get(key, [])
         return model
 
@@ -67,20 +90,20 @@ class IpypyManager(FileContentsManager):
         result = super().get(path, content, type, format)
         if type is None:
             type = self.guess_type(path)
-        if type != 'notebook':
+        elif type != 'notebook':
             return result
+        # Now only handling notebooks
         if content:
             # look for the outputs file
             outputs_uri = self._get_outputs_uri(path)
             if self.file_exists(outputs_uri):
-                outputs_data = self.get(outputs_uri, True, 'file')
+                outputs_data = super().get(outputs_uri, True, 'file')
                 result = self._merge_model(result, outputs_data)
         
         return result
 
     def is_hidden(self, path):
         """Is path a hidden directory or file?"""
-        if path.endswith('.ipynb.db'):
-            print('hide path', path)
+        if path.endswith(self.extension):
             return True
         return super().is_hidden(path)
