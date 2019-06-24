c8a16522c0f2156cbedd4b197a357145f27934ad
Javier Mansilla <javier.mansilla@mercadolibre.com>
2019-06-24 17:16:18 -0300

    adding url and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,16 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='ipypy',
     version='0.1.2',
-    url='',
+    url='https://github.com/jmansilla/ipypy',
     description='ipypy notebook format',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+  ],
 )
