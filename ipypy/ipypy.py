8f1b4c9f70afb4f88ab543e2138baf1739fd3729
Javier Mansilla <javier.mansilla@mercadolibre.com>
2019-06-24 17:18:57 -0300

    formatting code with black

--- ipypy/ipypy.py
+++ ipypy/ipypy.py
@@ -9,6 +9,7 @@ class SplitManager(FileContentsManager):
     """
     ContentsManager that persists data splitted
     """
+
     allow_hidden = False
 
     def guess_type(self, path, allow_directory=True):
@@ -17,20 +18,20 @@ class SplitManager(FileContentsManager):
         If allow_directory is False, don't consider the possibility that the
         file is a directory.
         """
-        if path.endswith('.ipynb'):
-            return 'notebook'
+        if path.endswith(".ipynb"):
+            return "notebook"
         elif allow_directory and self.dir_exists(path):
-            return 'directory'
+            return "directory"
         else:
-            return 'file'
+            return "file"
 
     def _get_splitted_uri(self, path):
-        return path.replace('.ipynb', self.extension)
+        return path.replace(".ipynb", self.extension)
 
     def save(self, model, path):
         """Save a file or directory model to path."""
         _type = self.guess_type(path)
-        if _type != 'notebook':
+        if _type != "notebook":
             return super().save(model, path)
         else:
             shallow_model, splitted = self._split_model(model)
@@ -42,16 +43,16 @@ class SplitManager(FileContentsManager):
         result = super().get(path, content, type, format)
         if type is None:
             type = self.guess_type(path)
-        elif type != 'notebook':
+        elif type != "notebook":
             return result
         # Now only handling notebooks
         if content:
             # look for the splitted file
             splitted_uri = self._get_splitted_uri(path)
             if self.file_exists(splitted_uri):
-                splitted_data = super().get(splitted_uri, True, 'file')
+                splitted_data = super().get(splitted_uri, True, "file")
                 result = self._merge_model(result, splitted_data)
-        
+
         return result
 
     def is_hidden(self, path):
@@ -65,62 +66,63 @@ class SplitCodeManager(SplitManager):
     """
     ContentsManager that persists code to a pure code file.
     """
-    extension = '.ipypy'
-    comment_prefix = '# <cell-init>'
+
+    extension = ".ipypy"
+    comment_prefix = "# <cell-init>"
 
     @classmethod
     def _comment(cls, key):
         # Hardcoded python comment
-        return f'{cls.comment_prefix} cell-id: {key}'
+        return f"{cls.comment_prefix} cell-id: {key}"
 
     def _split_model(self, model):
         shallow_model = deepcopy(model)
-        splitted = {'type': 'file', 'format': 'text', 'content': {}}
-        prefix = 'some-hash'
-        for i, cell in enumerate(shallow_model['content']['cells']):
-            if not cell['cell_type'] == 'code':
+        splitted = {"type": "file", "format": "text", "content": {}}
+        prefix = "some-hash"
+        for i, cell in enumerate(shallow_model["content"]["cells"]):
+            if not cell["cell_type"] == "code":
                 continue
-            key = f'{prefix}-{i}'
-            splitted['content'][key] = cell['source']
+            key = f"{prefix}-{i}"
+            splitted["content"][key] = cell["source"]
             self._format_cell_after_split(cell, key)
-        
+
         # lets format the file now
         code_lines = []
-        for k, value in splitted['content'].items():
+        for k, value in splitted["content"].items():
             code_lines.append(self._comment(k))
             if isinstance(value, (list, tuple)):
                 code_lines.extend(value)
             else:
                 code_lines.append(value)
-        splitted['content'] = '\n'.join(code_lines)
+        splitted["content"] = "\n".join(code_lines)
         return shallow_model, splitted
 
     @staticmethod
     def _format_cell_after_split(cell, key):
-        cell['source'] = ''
-        cell['metadata']['code_id'] = key
+        cell["source"] = ""
+        cell["metadata"]["code_id"] = key
 
     @staticmethod
     def _get_split_key(cell):
-        if not cell['cell_type'] == 'code':
+        if not cell["cell_type"] == "code":
             return None
-        return cell.get('metadata', {}).get('code_id')
+        return cell.get("metadata", {}).get("code_id")
 
     def _parse_source_data(self, data):
-        chunks = data.get('content', '').split(self.comment_prefix)
+        chunks = data.get("content", "").split(self.comment_prefix)
         code = {}
         detached_code = []  # someone inserted new cells directly in code file
         for chunk in chunks:
             if not chunk:
                 continue
             head, *lines = chunk.splitlines(keepends=True)
-            if ':' in head:
-                key = head.split(':')[1].strip()
+            if ":" in head:
+                key = head.split(":")[1].strip()
             else:
                 key = None
             if lines:
                 last_line = lines[-1]
-                if last_line.endswith('\n'):  # there is always an extra newline added
+                if last_line.endswith("\n"):  # there is always an extra newline added
                     lines[-1] = last_line[:-1]  # removed extra
             if key:
                 code[key] = lines
@@ -132,19 +134,20 @@ class SplitCodeManager(SplitManager):
         model = deepcopy(shallow_model)
         code, detached_code = self._parse_source_data(splitted_model)
 
-        for cell in model['content']['cells']:
+        for cell in model["content"]["cells"]:
             key = self._get_split_key(cell)
             if key is None or key not in code:
                 continue
-            cell['source'] = code.pop(key)
+            cell["source"] = code.pop(key)
         new_cells = list(code.items())  # unused source lines
         new_cells += zip(repeat(None), detached_code)
         for extra_key, source_lines in new_cells:
-            model['content']['cells'].append(
-                { "cell_type" : "code",
-                  "metadata" : {'code_id': extra_key},
-                  "source" : source_lines,
-                  "outputs": [],
+            model["content"]["cells"].append(
+                {
+                    "cell_type": "code",
+                    "metadata": {"code_id": extra_key},
+                    "source": source_lines,
+                    "outputs": [],
                 }
             )
         return model
@@ -154,46 +157,42 @@ class SplitOutputManager(FileContentsManager):
     """
     ContentsManager that persists output to a different file.
     """
+
     allow_hidden = False
-    extension = '.nbout'
+    extension = ".nbout"
 
     def _split_model(self, model):
         shallow_model = deepcopy(model)
-        outputs = {'type': 'file', 'format': 'text', 'content': {}}
-        prefix = 'some-hash'
-        for i, cell in enumerate(shallow_model['content']['cells']):
-            if not cell.get('outputs', []):
+        outputs = {"type": "file", "format": "text", "content": {}}
+        prefix = "some-hash"
+        for i, cell in enumerate(shallow_model["content"]["cells"]):
+            if not cell.get("outputs", []):
                 continue
-            key = f'{prefix}-{i}'
-            outputs['content'][key] = cell['outputs']
-            cell['outputs'] = self._build_external_output(key)
-        outputs['content'] = json.dumps(
-            outputs['content'], sort_keys=True, indent=4)
+            key = f"{prefix}-{i}"
+            outputs["content"][key] = cell["outputs"]
+            cell["outputs"] = self._build_external_output(key)
+        outputs["content"] = json.dumps(outputs["content"], sort_keys=True, indent=4)
         return shallow_model, outputs
-    
+
     @staticmethod
     def _get_external_output_id(cell):
-        if not cell['outputs']:
+        if not cell["outputs"]:
             return None
-        out_0 = cell['outputs'][0]
-        if out_0['output_type'] == 'external':
-            return out_0.get('id')
+        out_0 = cell["outputs"][0]
+        if out_0["output_type"] == "external":
+            return out_0.get("id")
         return None
 
     @staticmethod
     def _build_external_output(key):
-        return [
-            {'output_type': 'external',
-             'id': key
-            }
-        ]
+        return [{"output_type": "external", "id": key}]
 
     def _merge_model(self, shallow_model, outputs_model):
         model = deepcopy(shallow_model)
-        outputs = json.loads(outputs_model.get('content', '{}'))
-        for cell in model['content']['cells']:
+        outputs = json.loads(outputs_model.get("content", "{}"))
+        for cell in model["content"]["cells"]:
             key = self._get_external_output_id(cell)
             if key is None:
                 continue
-            cell['outputs'] = outputs.get(key, [])
+            cell["outputs"] = outputs.get(key, [])
         return model
--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from setuptools import setup, find_packages
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='ipypy',
-    version='0.1.2',
-    url='https://github.com/jmansilla/ipypy',
-    description='ipypy notebook format',
+    name="ipypy",
+    version="0.1.2",
+    url="https://github.com/jmansilla/ipypy",
+    description="ipypy notebook format",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3',
-  ],
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+    ],
 )
